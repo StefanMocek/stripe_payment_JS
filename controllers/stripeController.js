07e4de46358aaa0cbf539292153d6440b99cd609
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-04-28 18:05:10 +0200

    final controler

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -1,5 +1,17 @@
-const stripe = async (req, res) => {
-  res.send('stripe route')
-}
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-module.exports = stripe
+const stripeController = async (req, res) => {
+  const {purchase, total_amount, shipping_fee} = req.body;
+  const calculateOrderAmount = () => {
+    return total_amount + shipping_fee
+  };
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(),
+    currency: 'pln'
+  });
+
+  res.json({clientSecret: paymentIntent.client_secret})
+};
+
+module.exports = stripeController
